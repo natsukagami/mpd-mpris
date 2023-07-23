393c2165ab23f8710cda2bd4f63ce9c2ef83cca1
Natsu Kagami <nki@nkagami.me>
2023-07-23 11:43:40 +0200

    Nix package: add .desktop and .service file

--- nix/package.nix
+++ nix/package.nix
@@ -3,4 +3,13 @@ buildGoModule {
   name = "mpd-mpris";
   src = ./..;
   vendorSha256 = "sha256-HCDJrp9WFB1z+FnYpOI5e/AojtdnpN2ZNtgGVaH/v/Q=";
+
+  postInstall = ''
+    mkdir -p $out/lib/systemd/user
+    substitute mpd-mpris.service $out/lib/systemd/user/mpd-mpris.service \
+       --replace "/usr/bin/mpd-mpris" "$out/bin/mpd-mpris"
+
+    mkdir -p $out/etc/xdg/autostart
+    substitute mpd-mpris.desktop $out/etc/xdg/autostart/mpd-mpris.desktop
+  '';
 }
