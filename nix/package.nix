e764577519eaeb791c67605336a303888001c7dd
Natsu Kagami <nki@nkagami.me>
2024-06-02 16:35:41 +0200

    Update flake
    Fixes #54 by changing `vendorSha256` to `vendorHash`.

--- nix/package.nix
+++ nix/package.nix
@@ -1,8 +1,8 @@
-{ pkgs, buildGoModule }:
+{ buildGoModule }:
 buildGoModule {
   name = "mpd-mpris";
   src = ./..;
-  vendorSha256 = "sha256-HCDJrp9WFB1z+FnYpOI5e/AojtdnpN2ZNtgGVaH/v/Q=";
+  vendorHash = "sha256-HCDJrp9WFB1z+FnYpOI5e/AojtdnpN2ZNtgGVaH/v/Q=";
 
   postInstall = ''
     mkdir -p $out/lib/systemd/user
